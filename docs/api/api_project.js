c0d46b33b5ed6301f3961026c32a71abc97b545f
Jeremy Harris <jeremy.harris@zenosmosis.com>
2018-05-28 17:45:21 -0600

    Re-added docs to source

---
+++ docs/api/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-05-28T23:45:06.875Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
